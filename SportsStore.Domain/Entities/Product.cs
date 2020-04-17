8dad6e51881bcf3ddaaeeaab29a374106f55ce70
sw1423 <sw1423@live.com>
2020-04-17 17:20:48 +0800

    update

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace SportsStore.Domain.Entities
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal? Price { get; set; }
+        public decimal Price { get; set; }
         public string Category { get; set; }
     }
 }
--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -138,5 +138,117 @@ result.ToString());
             //断言
             Assert.AreEqual(categoryToSelect, result);
         }
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            //准备-创建一些测试产品
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            //准备一创建一个新的购物车
+            Cart target = new Cart();
+            //动作
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+            //断言
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //准备一创建一些测试产品
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product
+            {
+                ProductID = 2,
+                Name = "P2"
+            };
+            //准备一创建新购物车
+            Cart target = new Cart();
+            //动作
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+            //断言
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            //准备一创建一些测试产品
+            Product p1 = new Product
+            {
+                ProductID = 1,
+                Name = "P1"
+            };
+            Product p2 = new Product
+            {
+                ProductID = 2,
+                Name = "P2"
+            };
+            Product p3 = new Product
+            {
+                ProductID = 3,
+                Name = "P3"
+            };
+            //准备一创建一个新的购物车
+            Cart target = new Cart();
+            //准备一对购物车添加一-些产品
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+            //动作
+            target.Removeline(p2);
+            //断言
+            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
+        }
+        public void Calculate_Cart_Total()
+        {
+            //准备一-创建一些测试产品
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+            //准备一创建一个新的购物车
+            Cart target = new Cart();
+            //动作
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+            decimal result = target.ComputeTotalValue();
+            //断言
+            Assert.AreEqual(result, 450M);
+        }
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            //准备一创建一些测试产品
+            Product p1 = new Product
+            {
+                ProductID = 1,
+                Name = "P1",
+                Price = 100M
+            };
+            Product p2 = new Product
+            {
+                ProductID = 2,
+                Name = "P2",
+                Price = 50M
+            };
+            //准备一创建一个新的购物车
+            Cart target = new Cart();
+            //准备一添加一些物品
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            //动作一重置购物车
+            target.Clear();
+            //断言
+            Assert.AreEqual(target.Lines.Count(), 0);
+        }
     }
 }
